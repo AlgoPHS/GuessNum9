e5672303400c91a1616df208ea027ab22edc8a76
Will Kinney <williamdkinney@gmail.com>
2017-01-25 13:41:43 -0500

    added scanner and txt for real this time

--- GuessNum9.java
+++ GuessNum9.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class GuessNum9 {
 	public static void main(String[] args){
 		
 		
+		Scanner sc = new Scanner(System.in);
+		int rand = (int)Math.random()*100; 
 		
-		
+		System.out.println("I am thinking of a number 1 through 100. \n Can you guess my number?");
 	}
 }
