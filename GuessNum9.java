44be8a84ff96a35ed9095e5e6d0de5e41bd51365
Will Kinney <williamdkinney@gmail.com>
2017-01-30 14:41:59 -0500

    added proper functionality for replaying and added header

--- GuessNum9.java
+++ GuessNum9.java
@@ -1,30 +1,46 @@
 import java.util.Scanner;
 
+// Will Kinney & Tommy Liu
+// 1/25
+// Implement this program to play the game of “guess the number”
+// If the player’s is incorrect, your program should loop until 
+// the player finally gets the number right. Your program should 
+// keep telling the player Too high or Too low to help the player 
+// “zero in” on the correct answer. After a game ends, the program 
+// should report the number of games the player played and prompt 
+// the user to enter “y” to play again or “n” to exit the game.
+
+
+
 public class GuessNum9 {
 	public static void main(String[] args){
 		
 		Scanner sc = new Scanner(System.in);
+		boolean play = true;
+		int count = 0;
 		
 		int rand = (int)(Math.random()*(100-1) + 1);
 		System.out.println(rand);
 		
 		System.out.println("I am thinking of a number 1 through 100. \n Can you guess my number?");
-		
-		for(int i = 0; i < 4; i++){
+
+		while(play){
+			count++;
 			int guess = sc.nextInt();
 			if(guess > rand) System.out.println("Too high. Try again");
 			if(guess < rand) System.out.println("Too low. Try again");
 			if(guess == rand){ 
-				System.out.println("Excellent! You guessed the number! You guessed in " + i + " tries");
+				System.out.println("Excellent! You guessed the number! You guessed in " + count + " tries");
 				System.out.println("Would you like to play again (y/n)?");
 				String response = sc.next();
 				if(response.equals("y")){
-					i = 0;
+					count = 0;
 					rand = (int)(Math.random()*(100-1) + 1);
 					System.out.println(rand);
-				if(response.equals("n")) break;
+					System.out.println("I am thinking of a number 1 through 100. \n Can you guess my number?");
+				}
+				if(response.equals("n")) play = false;
 				}
 			}
 		}
 	}
-}
